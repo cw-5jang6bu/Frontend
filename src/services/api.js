d918d01f88037701feabe5b8c9ff52bfa25da2e6
son <jongin718@gmail.com>
2025-02-24 22:07:07 +0900

    fix: url 수정

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://a2f3b47d8b1b9400ca2981031f0b9995-134946597.ap-northeast-2.elb.amazonaws.com";
+//const API_BASE_URL = "http://a2f3b47d8b1b9400ca2981031f0b9995-134946597.ap-northeast-2.elb.amazonaws.com";
 
-//const API_BASE_URL = "http://localhost:8080"
+const API_BASE_URL = "http://localhost:8080"
 
 const api = axios.create({
     baseURL: API_BASE_URL,
