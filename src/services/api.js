6f7a3d6b9eeb8c53627560718a4e0e03adc8c7ed
son <jongin718@gmail.com>
2025-02-24 23:25:39 +0900

    fix: login 구조 변경

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-//const API_BASE_URL = "http://a2f3b47d8b1b9400ca2981031f0b9995-134946597.ap-northeast-2.elb.amazonaws.com";
+const API_BASE_URL = "http://ac26a7732cb91416fbd6cea6edc0da4f-650605713.ap-northeast-2.elb.amazonaws.com";
 
-const API_BASE_URL = "http://localhost:8080"
+//const API_BASE_URL = "http://localhost:8080"
 
 const api = axios.create({
     baseURL: API_BASE_URL,
