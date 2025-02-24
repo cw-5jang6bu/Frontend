c67174c62414fbcd7a8bc2391fdd2d629b4568c0
son <jongin718@gmail.com>
2025-02-24 18:16:19 +0900

    fix: URL 업데이트

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,12 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://member-svc.member.svc.cluster.local:8080";
+const API_BASE_URL = "http://member-service.member.svc.cluster.local:8080";
 
 const api = axios.create({
     baseURL: API_BASE_URL,
     headers: {
         "Content-Type": "application/json",
     },
-});
-
-// 요청 인터셉터: JWT 토큰 자동 추가
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
 });
 
 export default api;
