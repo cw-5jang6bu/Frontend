8b1e32da261f362ece0b0c3b569f6017dc3cf734
son <jongin718@gmail.com>
2025-02-25 23:12:41 +0900

    fix: 배포 환경 설정

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // ✅ 백엔드 API 주소를 직접 하드코딩 (EKS 내부 DNS 사용)
-const API_BASE_URL = "http://member-service.member.svc.cluster.local:8080";
+const API_BASE_URL = "http://member-service.member.svc.cluster.local";
 
 const api = axios.create({
     baseURL: API_BASE_URL,
