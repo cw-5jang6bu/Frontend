ef0a20937d2616824fec1e6469af440b4716d86a
son <jongin718@gmail.com>
2025-02-25 11:35:20 +0900

    fix: ingress.yaml 파일 수정

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://ac26a7732cb91416fbd6cea6edc0da4f-650605713.ap-northeast-2.elb.amazonaws.com";
+//const API_BASE_URL = "http://ac26a7732cb91416fbd6cea6edc0da4f-650605713.ap-northeast-2.elb.amazonaws.com";
 
-//const API_BASE_URL = "http://localhost:8080"
+const API_BASE_URL = "http://localhost:8080"
 
 const api = axios.create({
     baseURL: API_BASE_URL,
